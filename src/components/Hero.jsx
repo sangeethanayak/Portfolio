598cdfac30f2066222b82d6fe27f1462e2af2097
sangeetha nayak <sangeethanayak333@gmail.com>
2023-03-18 21:15:35 +0530

    initial commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,5 @@
 import { motion } from 'framer-motion';
 import { styles } from '../styles';
-import { ComputersCanvas } from './canvas';
 import { SectionWrapper } from "../hoc";
 import Typewriter from "typewriter-effect";
 const Hero = () => {
